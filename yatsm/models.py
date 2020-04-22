fe71c42a087977b3abcdf5deccc1783f85292cdc
Daniel Santos <daniel.martins@lumedigital.com.br>
2020-04-22 02:12:42 -0300

    #5: Added jwt authenticatio mechanism

--- yatsm/models.py
+++ yatsm/models.py
@@ -25,3 +25,23 @@ class Job(BaseModel):
             if param not in sign.parameters:
                 raise ValueError(f"Invalid job parameter [{param}]")
         return value
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: str = None
+
+
+class User(BaseModel):
+    username: str
+    email: str = None
+    full_name: str = None
+    disabled: bool = None
+
+
+class UserInDB(User):
+    hashed_password: str
