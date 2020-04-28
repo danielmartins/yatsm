8b67f45cb3086a87c44f71a8481157333673e3e1
Daniel Santos <daniel.martins@lumedigital.com.br>
2020-04-28 01:07:47 -0300

    Added authorize info

--- yatsm/main.py
+++ yatsm/main.py
@@ -4,6 +4,7 @@ from datetime import timedelta
 from dramatiq import Actor
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.logger import logger
+from fastapi.openapi.utils import get_openapi
 from fastapi.security import OAuth2PasswordRequestForm
 from starlette import status
 
@@ -32,7 +33,7 @@ async def get_available_jobs():
     )
 
 
-@app.post("/job")
+@app.post("/job", description="Dispara novo job para execução")
 async def add_job(
     job: models.Job, current_user: models.User = Depends(auth.get_current_active_user)
 ):
@@ -40,7 +41,7 @@ async def add_job(
     return {"job_id": job.id}
 
 
-@app.get("/job/{job_id}")
+@app.get("/job/{job_id}", description="Consulta resultado do job")
 async def get_job(
     job_id: str, current_user: models.User = Depends(auth.get_current_active_user)
 ):
@@ -54,7 +55,7 @@ async def get_job():
     return available_jobs
 
 
-@app.post("/token", response_model=models.Token)
+@app.post("/token", response_model=models.Token, description="Obter token de acesso")
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
     user = auth.authenticate_user(
         auth.fake_users_db, form_data.username, form_data.password
@@ -70,3 +71,27 @@ async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(
         data={"sub": user.username}, expires_delta=access_token_expires
     )
     return {"access_token": access_token, "token_type": "bearer"}
+
+
+def custom_openapi():
+
+    api_description = """API para execução de Jobs em Background, para realizar o login use o 
+    botão authorize. 
+    Login/senha: johndoe/123qweasd"""
+
+    if app.openapi_schema:
+        return app.openapi_schema
+    openapi_schema = get_openapi(
+        title="Execução de jobs",
+        version="0.1.0",
+        description=api_description,
+        routes=app.routes,
+    )
+    openapi_schema["info"]["x-logo"] = {
+        "url": "https://fastapi.tiangolo.com/img/logo-margin/logo-teal.png"
+    }
+    app.openapi_schema = openapi_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_openapi
