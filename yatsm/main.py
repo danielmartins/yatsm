5472dbf590225a91858150ab63b615dba3fd866f
Daniel Santos <daniel.martins@lumedigital.com.br>
2020-04-28 00:56:17 -0300

    Added endpoint with available jobs
    Added improvements to docker-compose

--- yatsm/main.py
+++ yatsm/main.py
@@ -1,11 +1,13 @@
+import inspect
 from datetime import timedelta
 
+from dramatiq import Actor
 from fastapi import Depends, FastAPI, HTTPException
 from fastapi.logger import logger
 from fastapi.security import OAuth2PasswordRequestForm
 from starlette import status
 
-from yatsm import auth, models
+from yatsm import auth, jobs, models
 from yatsm.jobber import jobber
 from yatsm.scheduler import scheduler
 
@@ -13,6 +15,21 @@ app = FastAPI()
 
 logger.info("Starting scheduler in background")
 scheduler.start()
+
+available_jobs = {}
+
+excludes = ["success", "failure"]
+
+
+@app.on_event("startup")
+async def get_available_jobs():
+    available_jobs.update(
+        {
+            j: str(inspect.signature(getattr(jobs, j).fn))
+            for j in dir(jobs)
+            if isinstance(getattr(jobs, j), Actor) and j not in excludes
+        }
+    )
 
 
 @app.post("/job")
@@ -29,6 +46,12 @@ async def get_job(
 ):
     result = jobber.get_result(job_id)
     return {"job_id": job_id, "results": result}
+
+
+@app.get("/jobs/available")
+async def get_job():
+    print("available")
+    return available_jobs
 
 
 @app.post("/token", response_model=models.Token)
